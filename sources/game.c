0dd05185123b73971f549c452988bc4d52eee48e
davidstyrbjorn <davidstyrbjorn@gmail.com>
2021-11-11 11:22:28 +0100

    comment

--- sources/game.c
+++ sources/game.c
@@ -1,11 +1,15 @@
 #include"game.h"
 
 #include"raylib.h"
+#include"rlgl.h"
 
 void StartGame(s_game* game){
     InitWindow(game->window_size.x, game->window_size.y, game->title);
 
     RunGame(game); // Go further into it by starting the game loop
+
+    // TODO: Can we do shader stuff here? Setup a quad which covers the screen?
+    // Try out the rlgl abstraction framework
 }
 
 void RunGame(s_game* game) {
